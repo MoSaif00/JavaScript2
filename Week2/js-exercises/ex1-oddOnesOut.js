9f0cccb178207961dfe9902229f41313731a267e
MoSaif00 <moh97ab@hotmail.com>
2020-10-08 22:58:39 +0200

    used filter and map to re write oddOnesOut program

--- Week2/js-exercises/ex1-oddOnesOut.js
+++ Week2/js-exercises/ex1-oddOnesOut.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
 
  ** Exercise 1: The odd ones out **
@@ -7,15 +8,25 @@
  The function should still behave the same.
 
  */
-function doubleEvenNumbers(numbers) {
-  const newNumbers = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-      newNumbers.push(numbers[i] * 2);
-    }
-  }
-  return newNumbers;
+// function doubleEvenNumbers(numbers) {
+//   const newNumbers = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//       newNumbers.push(numbers[i] * 2);
+//     }
+//   }
+//   return newNumbers;
+// }
+
+
+/* Using the map and filter functions, rewrite the doubleEvenNumbers function. */
+function doubleEvenNumbers(numbers){
+  const newArray = numbers.filter((number) => number % 2 === 0 )
+                          .map((number)=> number * 2);
+                          
+  return newArray;
 }
 
 const myNumbers = [1, 2, 3, 4];
 console.log(doubleEvenNumbers(myNumbers)); // Logs "[4, 8]" to the console
+
