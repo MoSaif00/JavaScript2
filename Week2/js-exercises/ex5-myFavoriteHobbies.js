295ac8bbe0d134456ec9a2e6ebc3472b3da0dd44
MoSaif00 <moh97ab@hotmail.com>
2020-10-09 23:45:23 +0200

    finished myfavoritehobbies app and check it up , starting to do the weekly project

--- Week2/js-exercises/ex5-myFavoriteHobbies.js
+++ Week2/js-exercises/ex5-myFavoriteHobbies.js
@@ -10,7 +10,26 @@
 
 function createHTMLList(arr) {
   // your code goes in here
-}
+
+/* add header and style the page */
+  const header= document.createElement('h2');
+  document.body.appendChild(header);
+  header.innerText= 'my favorite hobbies';
+  header.style.textTransform = 'UpperCase';
+  header.style.textDecoration= ' underline ';
+  document.body.style.textAlign='center';
+
+  const hobbiesList = arr.forEach(element => {
+    const ulElement = document.createElement('ul'); // add unordered list 
+    const liElemets = document.createElement('li'); // add list item 
+    liElemets.innerText = element; // teke the paremeter and push the elements into the list items 
+    ulElement.appendChild(liElemets); // append li to Ul
+    document.body.appendChild(ulElement); // append Ul list to body tag 
+    liElemets.style.listStyle='none';
+  });  
+  return hobbiesList;
+
+};
 
 const myHobbies = [
   'Meditation',
@@ -19,3 +38,11 @@ const myHobbies = [
   'Hanging out with friends',
   'Going to the gym',
 ];
+createHTMLList(myHobbies);
+
+// const mySkills = [
+//   'programming',
+//   'debugging',
+//   'troubleshoting'
+// ];
+// createHTMLList(mySkills);
