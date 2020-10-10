f1832d6c80603792674d72314145c3ca445359a1
MoSaif00 <moh97ab@hotmail.com>
2020-10-10 21:51:40 +0200

    made some changes as suggested by Wilgert , and tried to use the function with out for loop and wow , it worked . thanks to Wilgert

--- Week1/project/index.js
+++ Week1/project/index.js
@@ -4,27 +4,27 @@
 const quotesList = [
     {
         quote: '"Get busy living or get busy dying."',
-        author: '– Stephen King ',
+        author: 'Stephen King ',
     },
     {
         quote: '"Great minds discuss ideas; average minds discuss events; small minds discuss people.”',
-        author: '– Eleanor Roosevelt',
+        author: 'Eleanor Roosevelt',
     },
     {
         quote: '"Those who dare to fail miserably can achieve greatly."',
-        author: '– John F. Kennedy ',
+        author: 'John F. Kennedy ',
     },
     {
         quote: '"It is hard to fail, but it is worse never to have tried to succeed."',
-        author: '– Theodore Roosevelt ',
+        author: 'Theodore Roosevelt ',
     },
     {
         quote: '"Let us always meet each other with smile, for the smile is the beginning of love."',
-        author: '– Mother Teresa ',
+        author: 'Mother Teresa ',
     },
     {
         quote: '"Love is a serious mental disease."',
-        author: '– Plato',
+        author: 'Plato',
     },
     
 ];
@@ -35,10 +35,10 @@ clickButton.addEventListener('click', findNewQuote);
 function findNewQuote(){
     const quoteName = document.getElementById('quoteID');
     const authorName = document.getElementById('authorID');
-    for (let i = 0; i < quotesList.length; i++) {
+    // for (let i = 0; i < quotesList.length; i++) {
         const randomQuotes = quotesList[Math.floor(Math.random()*quotesList.length)];
         quoteName.innerText=  randomQuotes.quote;
-        authorName.innerText= randomQuotes.author;       
-    }
+        authorName.innerText= ` - ${randomQuotes.author}`;       
+    // }
 }
 findNewQuote();
