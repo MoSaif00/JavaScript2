b4f979d9fe7d3e162173a3ee3424cbd2a38c269e
MoSaif00 <moh97ab@hotmail.com>
2020-10-18 21:07:40 +0200

    i guessed the output of exercise 3

--- Week3/js-exercises/ex3-GuessTheOutput.js
+++ Week3/js-exercises/ex3-GuessTheOutput.js
@@ -8,6 +8,13 @@ Write out your reasoning in 50 words or less.
 
 */
 
+// The output will alert along with the number 12. Because  inside the local function has the value of a assigned with number 12, and when it returns closure function which has alert method.
+/*
+- Frist a variable declared with type of value number which is 10, after that x is assigned with immediately invoked function .
+- inside the function the value of a will be changed to 12 . and the function will return closure function.
+- the returned funcion has an alert methond assigned to a variable.
+- x() will be called and that will call the function that assigned to it which is closure so it will alert with the value of a which is inside the closure . 
+*/
 
 
 let a = 10;
