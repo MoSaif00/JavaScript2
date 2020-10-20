6d916c08e5d898191943498b598a4a8542748851
MoSaif00 <moh97ab@hotmail.com>
2020-10-20 21:30:00 +0200

    Reviewed the project and made some changes , and ready to be used

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -11,8 +11,9 @@ const outputMoneySymbol = document.getElementById('amount-sign');
 const footerTotalAmount = document.getElementById('total-amount-brief');
 const footerTipAmount = document.getElementById('tip-brief');
 const footerSumAll = document.getElementById('total-brief');
+const billDetails = document.querySelector('.bill-detail p');
 
-
+// function 
 function calculateTip(){
     let totalAmount = Number(amountInput.value);
     let serviceRate = Number(serviceRateInput.value)/100;
@@ -20,24 +21,24 @@ function calculateTip(){
     let moneySign =moneySymbol.value;
     let tipAmount = (totalAmount * serviceRate)/numberOfPeople;
     tipAmount =tipAmount.toFixed(2);
-
     displayTip.innerText = tipAmount ; 
+    
+    // append the symbol of dollar or euro and display brief of the total amount + the tip 
     outputMoneySymbol.textContent = moneySign;
     footerTotalAmount.textContent = totalAmount;
     footerTipAmount.textContent=tipAmount;
     footerSumAll.textContent= totalAmount + (tipAmount*numberOfPeople);
+    billDetails.style.visibility='visible';
 
-
+    //If there's only 1 person who shares the bill, output only the tip amount (omit the "each")
     const plural = numberOfPeople >= 2 ? eachForPlural.style.visibility = 'visible' : eachForPlural.style.visibility = 'hidden' ;
-
 };
 
-
-
 calculateButton.addEventListener('click',function checkInput(){
+    //If any of the input fields are empty when the button is clicked, call an alert that says: "You need to fill in all the fields!"
     if(amountInput.value === "" || serviceRateInput.value === "" || numberOfPeopleInput.value === ""){
         alert('Please fill the empty fields');
     }else{
-        calculateTip();
+        calculateTip(); // call back the function that calculate the tip
     }
 });
